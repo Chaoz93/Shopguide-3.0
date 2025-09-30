2f391fcf165f883942b8ec6ad729abdc583a11be
Luna Biederstädt <133014635+ChaozMedia@users.noreply.github.com>
2025-09-30 08:57:18 +0200

    Add Kantine module with embedded canteen iframe

---
+++ modules/Kantine/Kantine.js
@@ -0,0 +1,14 @@
+window.renderKantine = function renderKantine(root) {
+  root.style.display = 'flex';
+  root.style.alignItems = 'stretch';
+  root.style.justifyContent = 'stretch';
+  root.innerHTML = `
+    <iframe
+      src="https://lufthansagroup-taste-and-more.signage-server.de/html-export/hamburg-betriebsrestaurant-1-de/index.html"
+      title="Speiseplan – LHT Betriebsrestaurant 1"
+      loading="lazy"
+      referrerpolicy="no-referrer"
+      style="border:0;width:100%;height:100%;flex:1;"
+    ></iframe>
+  `;
+};
