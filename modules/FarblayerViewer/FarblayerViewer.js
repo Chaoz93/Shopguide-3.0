f09c8012b867a6300a4d9a1f95c6376b3fb66bd9
Chaoz93 <131708348+Chaoz93@users.noreply.github.com>
2025-10-16 14:22:46 +0200

    Remove redundant test UI from Farblayer modal

--- modules/FarblayerViewer/FarblayerViewer.js
+++ modules/FarblayerViewer/FarblayerViewer.js
@@ -1404,7 +1404,6 @@
                 <button class="flv-group-btn" type="button" data-flv-add-group>+ Gruppe hinzufügen</button>
                 <button class="flv-group-btn" type="button" data-flv-remove-group>– Gruppe entfernen</button>
               </div>
-              <div class="flv-test-ui" data-flv-test-ui></div>
             </div>
           </div>
           <div class="flv-footer">
@@ -1439,7 +1438,6 @@
   const addGroupBtn = root.querySelector('[data-flv-add-group]');
   const removeGroupBtn = root.querySelector('[data-flv-remove-group]');
   const mainTestUIContainer = root.querySelector('[data-flv-main-ui]');
-  const testUIContainer = root.querySelector('[data-flv-test-ui]');
   const saveBtn = root.querySelector('[data-flv-save]');
   const cancelBtn = root.querySelector('[data-flv-cancel]');
   const assignModeBtn = root.querySelector('[data-flv-assign-toggle]');
@@ -1465,10 +1463,6 @@
         { id: 'btn-help', label: 'Hilfe öffnen' }
       ]
     });
-  }
-
-  if(testUIContainer){
-    renderTestUI(testUIContainer);
   }
 
   if(assignModeBtn){
